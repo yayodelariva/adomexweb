3d620166518868e942192d651de6721f330f6a59
Yayodelariva <eduardodlrp@outlook.com>
2024-07-09 00:15:24 -0600

    add grid to eventos & liga mex de dodgeball add js functionality to grids

---
+++ eventos/script.js
@@ -0,0 +1,64 @@
+const eCard1Year = document.querySelector(".eCard1 > .dateWrapper > .year");
+const eCard1Month = document.querySelector(".eCard1 > .dateWrapper > .month");
+const eCard1Day = document.querySelector(".eCard1 > .dateWrapper > .day");
+const eCard2Year = document.querySelector(".eCard2 > .dateWrapper > .year");
+const eCard2Month = document.querySelector(".eCard2 > .dateWrapper > .month");
+const eCard2Day = document.querySelector(".eCard2 > .dateWrapper > .day");
+const eCard3Year = document.querySelector(".eCard3 > .dateWrapper > .year");
+const eCard3Month = document.querySelector(".eCard3 > .dateWrapper > .month");
+const eCard3Day = document.querySelector(".eCard3 > .dateWrapper > .day");
+
+const eCard1EventName = document.querySelector(
+  ".eCard1 > .infoWrapper > .eventName"
+);
+const eCard1EventDuration = document.querySelector(
+  ".eCard1 > .infoWrapper > .eventDuration"
+);
+const eCard1EventLocation = document.querySelector(
+  ".eCard1 > .infoWrapper > .eventLocation"
+);
+const eCard2EventName = document.querySelector(
+  ".eCard2 > .infoWrapper > .eventName"
+);
+const eCard2EventDuration = document.querySelector(
+  ".eCard2 > .infoWrapper > .eventDuration"
+);
+const eCard2EventLocation = document.querySelector(
+  ".eCard2 > .infoWrapper > .eventLocation"
+);
+const eCard3EventName = document.querySelector(
+  ".eCard3 > .infoWrapper > .eventName"
+);
+const eCard3EventDuration = document.querySelector(
+  ".eCard3 > .infoWrapper > .eventDuration"
+);
+const eCard3EventLocation = document.querySelector(
+  ".eCard3 > .infoWrapper > .eventLocation"
+);
+
+//EVENT CARD #1
+eCard1Year.innerHTML = "2069";
+eCard1Month.innerHTML = "Ene";
+eCard1Day.innerHTML = "15";
+
+eCard1EventName.innerHTML = "Posada Navidena";
+eCard1EventDuration.innerHTML = "Ago 21-23";
+eCard1EventLocation.innerHTML = "CDMX";
+
+//EVENT CARD #2
+eCard2Year.innerHTML = "2002";
+eCard2Month.innerHTML = "Jul";
+eCard2Day.innerHTML = "9";
+
+eCard2EventName.innerHTML = "Karaoke Dodgeballero";
+eCard2EventDuration.innerHTML = "Jul 9-11";
+eCard2EventLocation.innerHTML = "CDMX";
+
+//EVENT CARD #3
+eCard3Year.innerHTML = "2014";
+eCard3Month.innerHTML = "Sep";
+eCard3Day.innerHTML = "31";
+
+eCard3EventName.innerHTML = "Rifa de Verano";
+eCard3EventDuration.innerHTML = "Sep 31";
+eCard3EventLocation.innerHTML = "CDMX";
