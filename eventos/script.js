2e69b9991fdccd0d491e80531b8f5039e8f1ad33
Yayodelariva <eduardodlrp@outlook.com>
2024-12-03 11:42:26 -0600

    add more event cards

--- eventos/script.js
+++ eventos/script.js
@@ -7,6 +7,114 @@ const eCard2Day = document.querySelector(".eCard2 > .dateWrapper > .day");
 const eCard3Year = document.querySelector(".eCard3 > .dateWrapper > .year");
 const eCard3Month = document.querySelector(".eCard3 > .dateWrapper > .month");
 const eCard3Day = document.querySelector(".eCard3 > .dateWrapper > .day");
+const eCard4Year = document.querySelector(".eCard4 > .dateWrapper > .year");
+const eCard4Month = document.querySelector(".eCard4 > .dateWrapper > .month");
+const eCard4Day = document.querySelector(".eCard4 > .dateWrapper > .day");
+const eCard4EventName = document.querySelector(
+  ".eCard4 > .infoWrapper > .eventName"
+);
+const eCard4EventDuration = document.querySelector(
+  ".eCard4 > .infoWrapper > .eventDuration"
+);
+const eCard4EventLocation = document.querySelector(
+  ".eCard4 > .infoWrapper > .eventLocation"
+);
+const eCard5Year = document.querySelector(".eCard5 > .dateWrapper > .year");
+const eCard5Month = document.querySelector(".eCard5 > .dateWrapper > .month");
+const eCard5Day = document.querySelector(".eCard5 > .dateWrapper > .day");
+const eCard5EventName = document.querySelector(
+  ".eCard5 > .infoWrapper > .eventName"
+);
+const eCard5EventDuration = document.querySelector(
+  ".eCard5 > .infoWrapper > .eventDuration"
+);
+const eCard5EventLocation = document.querySelector(
+  ".eCard5 > .infoWrapper > .eventLocation"
+);
+const eCard6Year = document.querySelector(".eCard6 > .dateWrapper > .year");
+const eCard6Month = document.querySelector(".eCard6 > .dateWrapper > .month");
+const eCard6Day = document.querySelector(".eCard6 > .dateWrapper > .day");
+const eCard6EventName = document.querySelector(
+  ".eCard6 > .infoWrapper > .eventName"
+);
+const eCard6EventDuration = document.querySelector(
+  ".eCard6 > .infoWrapper > .eventDuration"
+);
+const eCard6EventLocation = document.querySelector(
+  ".eCard6 > .infoWrapper > .eventLocation"
+);
+const eCard7Year = document.querySelector(".eCard7 > .dateWrapper > .year");
+const eCard7Month = document.querySelector(".eCard7 > .dateWrapper > .month");
+const eCard7Day = document.querySelector(".eCard7 > .dateWrapper > .day");
+const eCard7EventName = document.querySelector(
+  ".eCard7 > .infoWrapper > .eventName"
+);
+const eCard7EventDuration = document.querySelector(
+  ".eCard7 > .infoWrapper > .eventDuration"
+);
+const eCard7EventLocation = document.querySelector(
+  ".eCard7 > .infoWrapper > .eventLocation"
+);
+const eCard8Year = document.querySelector(".eCard8 > .dateWrapper > .year");
+const eCard8Month = document.querySelector(".eCard8 > .dateWrapper > .month");
+const eCard8Day = document.querySelector(".eCard8 > .dateWrapper > .day");
+const eCard8EventName = document.querySelector(
+  ".eCard8 > .infoWrapper > .eventName"
+);
+const eCard8EventDuration = document.querySelector(
+  ".eCard8 > .infoWrapper > .eventDuration"
+);
+const eCard8EventLocation = document.querySelector(
+  ".eCard8 > .infoWrapper > .eventLocation"
+);
+const eCard9Year = document.querySelector(".eCard9 > .dateWrapper > .year");
+const eCard9Month = document.querySelector(".eCard9 > .dateWrapper > .month");
+const eCard9Day = document.querySelector(".eCard9 > .dateWrapper > .day");
+const eCard9EventName = document.querySelector(
+  ".eCard9 > .infoWrapper > .eventName"
+);
+const eCard9EventDuration = document.querySelector(
+  ".eCard9 > .infoWrapper > .eventDuration"
+);
+const eCard9EventLocation = document.querySelector(
+  ".eCard9 > .infoWrapper > .eventLocation"
+);
+const eCard10Year = document.querySelector(".eCard10 > .dateWrapper > .year");
+const eCard10Month = document.querySelector(".eCard10 > .dateWrapper > .month");
+const eCard10Day = document.querySelector(".eCard10 > .dateWrapper > .day");
+const eCard10EventName = document.querySelector(
+  ".eCard10 > .infoWrapper > .eventName"
+);
+const eCard10EventDuration = document.querySelector(
+  ".eCard10 > .infoWrapper > .eventDuration"
+);
+const eCard10EventLocation = document.querySelector(
+  ".eCard10 > .infoWrapper > .eventLocation"
+);
+const eCard11Year = document.querySelector(".eCard11 > .dateWrapper > .year");
+const eCard11Month = document.querySelector(".eCard11 > .dateWrapper > .month");
+const eCard11Day = document.querySelector(".eCard11 > .dateWrapper > .day");
+const eCard11EventName = document.querySelector(
+  ".eCard11 > .infoWrapper > .eventName"
+);
+const eCard11EventDuration = document.querySelector(
+  ".eCard11 > .infoWrapper > .eventDuration"
+);
+const eCard11EventLocation = document.querySelector(
+  ".eCard11 > .infoWrapper > .eventLocation"
+);
+const eCard12Year = document.querySelector(".eCard12 > .dateWrapper > .year");
+const eCard12Month = document.querySelector(".eCard12 > .dateWrapper > .month");
+const eCard12Day = document.querySelector(".eCard12 > .dateWrapper > .day");
+const eCard12EventName = document.querySelector(
+  ".eCard12 > .infoWrapper > .eventName"
+);
+const eCard12EventDuration = document.querySelector(
+  ".eCard12 > .infoWrapper > .eventDuration"
+);
+const eCard12EventLocation = document.querySelector(
+  ".eCard12 > .infoWrapper > .eventLocation"
+);
 
 const eCard1EventName = document.querySelector(
   ".eCard1 > .infoWrapper > .eventName"
@@ -37,28 +145,109 @@ const eCard3EventLocation = document.querySelector(
 );
 
 //EVENT CARD #1
-eCard1Year.innerHTML = "2069";
-eCard1Month.innerHTML = "Ene";
-eCard1Day.innerHTML = "15";
+eCard1Year.innerText = "2069";
+eCard1Month.innerText = "Ene";
+eCard1Day.innerText = "15";
 
-eCard1EventName.innerHTML = "Posada Navidena";
-eCard1EventDuration.innerHTML = "Ago 21-23";
-eCard1EventLocation.innerHTML = "CDMX";
+eCard1EventName.innerText = "Posada Navidena";
+eCard1EventDuration.innerText = "Ago 21-23";
+eCard1EventLocation.innerText = "CDMX";
 
 //EVENT CARD #2
-eCard2Year.innerHTML = "2002";
-eCard2Month.innerHTML = "Jul";
-eCard2Day.innerHTML = "9";
+eCard2Year.innerText = "2002";
+eCard2Month.innerText = "Jul";
+eCard2Day.innerText = "9";
 
-eCard2EventName.innerHTML = "Karaoke Dodgeballero";
-eCard2EventDuration.innerHTML = "Jul 9-11";
-eCard2EventLocation.innerHTML = "CDMX";
+eCard2EventName.innerText = "Karaoke Dodgeballero";
+eCard2EventDuration.innerText = "Jul 9-11";
+eCard2EventLocation.innerText = "CDMX";
 
 //EVENT CARD #3
-eCard3Year.innerHTML = "2014";
-eCard3Month.innerHTML = "Sep";
-eCard3Day.innerHTML = "31";
+eCard3Year.innerText = "2014";
+eCard3Month.innerText = "Sep";
+eCard3Day.innerText = "31";
 
-eCard3EventName.innerHTML = "Rifa de Verano";
-eCard3EventDuration.innerHTML = "Sep 31";
-eCard3EventLocation.innerHTML = "CDMX";
+eCard3EventName.innerText = "Rifa de Verano";
+eCard3EventDuration.innerText = "Sep 31";
+eCard3EventLocation.innerText = "CDMX";
+
+//EVENT CARD #4
+eCard4Year.innerText = "2014";
+eCard4Month.innerText = "Sep";
+eCard4Day.innerText = "31";
+
+eCard4EventName.innerText = "Rifa de Verano";
+eCard4EventDuration.innerText = "Sep 31";
+eCard4EventLocation.innerText = "CDMX";
+
+//EVENT CARD #5
+eCard5Year.innerText = "2014";
+eCard5Month.innerText = "Sep";
+eCard5Day.innerText = "31";
+
+eCard5EventName.innerText = "Rifa de Verano";
+eCard5EventDuration.innerText = "Sep 31";
+eCard5EventLocation.innerText = "CDMX";
+
+//EVENT CARD #6
+eCard6Year.innerText = "2014";
+eCard6Month.innerText = "Sep";
+eCard6Day.innerText = "31";
+
+eCard6EventName.innerText = "Rifa de Verano";
+eCard6EventDuration.innerText = "Sep 31";
+eCard6EventLocation.innerText = "CDMX";
+
+//EVENT CARD #7
+eCard7Year.innerText = "2014";
+eCard7Month.innerText = "Sep";
+eCard7Day.innerText = "31";
+
+eCard7EventName.innerText = "Rifa de Verano";
+eCard7EventDuration.innerText = "Sep 31";
+eCard7EventLocation.innerText = "CDMX";
+
+//EVENT CARD #8
+eCard8Year.innerText = "2014";
+eCard8Month.innerText = "Sep";
+eCard8Day.innerText = "31";
+
+eCard8EventName.innerText = "Rifa de Verano";
+eCard8EventDuration.innerText = "Sep 31";
+eCard8EventLocation.innerText = "CDMX";
+
+//EVENT CARD #9
+eCard9Year.innerText = "2014";
+eCard9Month.innerText = "Sep";
+eCard9Day.innerText = "31";
+
+eCard9EventName.innerText = "Rifa de Verano";
+eCard9EventDuration.innerText = "Sep 31";
+eCard9EventLocation.innerText = "CDMX";
+
+//EVENT CARD #10
+eCard10Year.innerText = "2014";
+eCard10Month.innerText = "Sep";
+eCard10Day.innerText = "31";
+
+eCard10EventName.innerText = "Rifa de Verano";
+eCard10EventDuration.innerText = "Sep 31";
+eCard10EventLocation.innerText = "CDMX";
+
+//EVENT CARD #11
+eCard11Year.innerText = "2014";
+eCard11Month.innerText = "Sep";
+eCard11Day.innerText = "31";
+
+eCard11EventName.innerText = "Rifa de Verano";
+eCard11EventDuration.innerText = "Sep 31";
+eCard11EventLocation.innerText = "CDMX";
+
+//EVENT CARD #12
+eCard12Year.innerText = "2014";
+eCard12Month.innerText = "Sep";
+eCard12Day.innerText = "31";
+
+eCard12EventName.innerText = "Rifa de Verano";
+eCard12EventDuration.innerText = "Sep 31";
+eCard12EventLocation.innerText = "CDMX";
