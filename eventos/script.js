f8a95a18aa01be4e0fdae0d69f83dc20bd783509
Yayodelariva <eduardodlrp@outlook.com>
2025-01-13 12:59:26 -0600

    add real events

--- eventos/script.js
+++ eventos/script.js
@@ -116,6 +116,84 @@ const eCard12EventLocation = document.querySelector(
   ".eCard12 > .infoWrapper > .eventLocation"
 );
 
+const eCard13Year = document.querySelector(".eCard13 > .dateWrapper > .year");
+const eCard13Month = document.querySelector(".eCard13 > .dateWrapper > .month");
+const eCard13Day = document.querySelector(".eCard13 > .dateWrapper > .day");
+const eCard13EventName = document.querySelector(
+  ".eCard13 > .infoWrapper > .eventName"
+);
+const eCard13EventDuration = document.querySelector(
+  ".eCard13 > .infoWrapper > .eventDuration"
+);
+const eCard13EventLocation = document.querySelector(
+  ".eCard13 > .infoWrapper > .eventLocation"
+);
+
+const eCard14Year = document.querySelector(".eCard14 > .dateWrapper > .year");
+const eCard14Month = document.querySelector(".eCard14 > .dateWrapper > .month");
+const eCard14Day = document.querySelector(".eCard14 > .dateWrapper > .day");
+const eCard14EventName = document.querySelector(
+  ".eCard14 > .infoWrapper > .eventName"
+);
+const eCard14EventDuration = document.querySelector(
+  ".eCard14 > .infoWrapper > .eventDuration"
+);
+const eCard14EventLocation = document.querySelector(
+  ".eCard14 > .infoWrapper > .eventLocation"
+);
+
+const eCard15Year = document.querySelector(".eCard15 > .dateWrapper > .year");
+const eCard15Month = document.querySelector(".eCard15 > .dateWrapper > .month");
+const eCard15Day = document.querySelector(".eCard15 > .dateWrapper > .day");
+const eCard15EventName = document.querySelector(
+  ".eCard15 > .infoWrapper > .eventName"
+);
+const eCard15EventDuration = document.querySelector(
+  ".eCard15 > .infoWrapper > .eventDuration"
+);
+const eCard15EventLocation = document.querySelector(
+  ".eCard15 > .infoWrapper > .eventLocation"
+);
+
+const eCard16Year = document.querySelector(".eCard16 > .dateWrapper > .year");
+const eCard16Month = document.querySelector(".eCard16 > .dateWrapper > .month");
+const eCard16Day = document.querySelector(".eCard16 > .dateWrapper > .day");
+const eCard16EventName = document.querySelector(
+  ".eCard16 > .infoWrapper > .eventName"
+);
+const eCard16EventDuration = document.querySelector(
+  ".eCard16 > .infoWrapper > .eventDuration"
+);
+const eCard16EventLocation = document.querySelector(
+  ".eCard16 > .infoWrapper > .eventLocation"
+);
+
+const eCard17Year = document.querySelector(".eCard17 > .dateWrapper > .year");
+const eCard17Month = document.querySelector(".eCard17 > .dateWrapper > .month");
+const eCard17Day = document.querySelector(".eCard17 > .dateWrapper > .day");
+const eCard17EventName = document.querySelector(
+  ".eCard17 > .infoWrapper > .eventName"
+);
+const eCard17EventDuration = document.querySelector(
+  ".eCard17 > .infoWrapper > .eventDuration"
+);
+const eCard17EventLocation = document.querySelector(
+  ".eCard17 > .infoWrapper > .eventLocation"
+);
+
+const eCard18Year = document.querySelector(".eCard18 > .dateWrapper > .year");
+const eCard18Month = document.querySelector(".eCard18 > .dateWrapper > .month");
+const eCard18Day = document.querySelector(".eCard18 > .dateWrapper > .day");
+const eCard18EventName = document.querySelector(
+  ".eCard18 > .infoWrapper > .eventName"
+);
+const eCard18EventDuration = document.querySelector(
+  ".eCard18 > .infoWrapper > .eventDuration"
+);
+const eCard18EventLocation = document.querySelector(
+  ".eCard18 > .infoWrapper > .eventLocation"
+);
+
 const eCard1EventName = document.querySelector(
   ".eCard1 > .infoWrapper > .eventName"
 );
@@ -145,109 +223,163 @@ const eCard3EventLocation = document.querySelector(
 );
 
 //EVENT CARD #1
-eCard1Year.innerText = "2069";
-eCard1Month.innerText = "Ene";
-eCard1Day.innerText = "15";
+eCard1Year.innerText = "2025";
+eCard1Month.innerText = "Feb";
+eCard1Day.innerText = "1";
 
-eCard1EventName.innerText = "Posada Navidena";
-eCard1EventDuration.innerText = "Ago 21-23";
+eCard1EventName.innerText = "Torneo Chino";
+eCard1EventDuration.innerText = "Feb 1";
 eCard1EventLocation.innerText = "CDMX";
 
 //EVENT CARD #2
-eCard2Year.innerText = "2002";
-eCard2Month.innerText = "Jul";
-eCard2Day.innerText = "9";
+eCard2Year.innerText = "2025";
+eCard2Month.innerText = "Feb";
+eCard2Day.innerText = "18";
 
-eCard2EventName.innerText = "Karaoke Dodgeballero";
-eCard2EventDuration.innerText = "Jul 9-11";
-eCard2EventLocation.innerText = "CDMX";
+eCard2EventName.innerText = "Campeonato Mundial Infantil";
+eCard2EventDuration.innerText = "Feb 18-22";
+eCard2EventLocation.innerText = "Calgary";
 
 //EVENT CARD #3
-eCard3Year.innerText = "2014";
-eCard3Month.innerText = "Sep";
-eCard3Day.innerText = "31";
+eCard3Year.innerText = "2025";
+eCard3Month.innerText = "Feb/Mar";
+eCard3Day.innerText = "1";
 
-eCard3EventName.innerText = "Rifa de Verano";
-eCard3EventDuration.innerText = "Sep 31";
-eCard3EventLocation.innerText = "CDMX";
+eCard3EventName.innerText = "Visoría Selección Nacional";
+eCard3EventDuration.innerText = "Feb o Mar 1";
+eCard3EventLocation.innerText = "Cuautla, Morelos";
 
 //EVENT CARD #4
-eCard4Year.innerText = "2014";
-eCard4Month.innerText = "Sep";
-eCard4Day.innerText = "31";
+eCard4Year.innerText = "2025";
+eCard4Month.innerText = "Mar";
+eCard4Day.innerText = "8";
 
-eCard4EventName.innerText = "Rifa de Verano";
-eCard4EventDuration.innerText = "Sep 31";
-eCard4EventLocation.innerText = "CDMX";
+eCard4EventName.innerText = "Campeonato Estatal de Foam y Cloth";
+eCard4EventDuration.innerText = "Mar 8 y 9";
+eCard4EventLocation.innerText = "";
 
 //EVENT CARD #5
-eCard5Year.innerText = "2014";
-eCard5Month.innerText = "Sep";
-eCard5Day.innerText = "31";
+eCard5Year.innerText = "2025";
+eCard5Month.innerText = "Mar";
+eCard5Day.innerText = "29";
 
-eCard5EventName.innerText = "Rifa de Verano";
-eCard5EventDuration.innerText = "Sep 31";
+eCard5EventName.innerText = "Campamento de Preselección";
+eCard5EventDuration.innerText = "Mar 29 y 30";
 eCard5EventLocation.innerText = "CDMX";
 
 //EVENT CARD #6
-eCard6Year.innerText = "2014";
-eCard6Month.innerText = "Sep";
-eCard6Day.innerText = "31";
+eCard6Year.innerText = "2025";
+eCard6Month.innerText = "Abr";
+eCard6Day.innerText = "5";
 
-eCard6EventName.innerText = "Rifa de Verano";
-eCard6EventDuration.innerText = "Sep 31";
-eCard6EventLocation.innerText = "CDMX";
+eCard6EventName.innerText = "Campeonato Estatal Infantil";
+eCard6EventDuration.innerText = "Abr 5";
+eCard6EventLocation.innerText = "";
 
 //EVENT CARD #7
-eCard7Year.innerText = "2014";
-eCard7Month.innerText = "Sep";
-eCard7Day.innerText = "31";
+eCard7Year.innerText = "2025";
+eCard7Month.innerText = "Abr";
+eCard7Day.innerText = "26";
 
-eCard7EventName.innerText = "Rifa de Verano";
-eCard7EventDuration.innerText = "Sep 31";
-eCard7EventLocation.innerText = "CDMX";
+eCard7EventName.innerText = "Campeonato Estatal Juvenil";
+eCard7EventDuration.innerText = "Abr 26";
+eCard7EventLocation.innerText = "";
 
 //EVENT CARD #8
-eCard8Year.innerText = "2014";
-eCard8Month.innerText = "Sep";
-eCard8Day.innerText = "31";
+eCard8Year.innerText = "2025";
+eCard8Month.innerText = "May";
+eCard8Day.innerText = "2";
 
-eCard8EventName.innerText = "Rifa de Verano";
-eCard8EventDuration.innerText = "Sep 31";
-eCard8EventLocation.innerText = "CDMX";
+eCard8EventName.innerText = "Campamento Nacional Libre de Cloth";
+eCard8EventDuration.innerText = "May 2-4";
+eCard8EventLocation.innerText = "Cuautla, Morelos";
 
 //EVENT CARD #9
-eCard9Year.innerText = "2014";
-eCard9Month.innerText = "Sep";
-eCard9Day.innerText = "31";
+eCard9Year.innerText = "2025";
+eCard9Month.innerText = "May";
+eCard9Day.innerText = "16";
 
-eCard9EventName.innerText = "Rifa de Verano";
-eCard9EventDuration.innerText = "Sep 31";
+eCard9EventName.innerText = "Campamento Nacional Infantil";
+eCard9EventDuration.innerText = "May 16-18";
 eCard9EventLocation.innerText = "CDMX";
 
 //EVENT CARD #10
-eCard10Year.innerText = "2014";
-eCard10Month.innerText = "Sep";
-eCard10Day.innerText = "31";
+eCard10Year.innerText = "2025";
+eCard10Month.innerText = "May";
+eCard10Day.innerText = "30";
 
-eCard10EventName.innerText = "Rifa de Verano";
-eCard10EventDuration.innerText = "Sep 31";
-eCard10EventLocation.innerText = "CDMX";
+eCard10EventName.innerText = "Campamento Nacional Libre de Foam";
+eCard10EventDuration.innerText = "May 30-Jun 1";
+eCard10EventLocation.innerText = "";
 
 //EVENT CARD #11
-eCard11Year.innerText = "2014";
-eCard11Month.innerText = "Sep";
-eCard11Day.innerText = "31";
+eCard11Year.innerText = "2025";
+eCard11Month.innerText = "Jun";
+eCard11Day.innerText = "13";
 
-eCard11EventName.innerText = "Rifa de Verano";
-eCard11EventDuration.innerText = "Sep 31";
-eCard11EventLocation.innerText = "CDMX";
+eCard11EventName.innerText = "Campamento Selección Nacional";
+eCard11EventDuration.innerText = "Jun 13-15";
+eCard11EventLocation.innerText = "";
 
 //EVENT CARD #12
-eCard12Year.innerText = "2014";
-eCard12Month.innerText = "Sep";
-eCard12Day.innerText = "31";
+eCard12Year.innerText = "2025";
+eCard12Month.innerText = "Jul";
+eCard12Day.innerText = "11";
 
-eCard12EventName.innerText = "Rifa de Verano";
-eCard12EventDuration.innerText = "Sep 31";
-eCard12EventLocation.innerText = "CDMX";
+eCard12EventName.innerText = "Campamento Selección Nacional";
+eCard12EventDuration.innerText = "Jul 11-13";
+eCard12EventLocation.innerText = "";
+
+//EVENT CARD #13
+eCard13Year.innerText = "2025";
+eCard13Month.innerText = "Ago";
+eCard13Day.innerText = "8";
+
+eCard13EventName.innerText = "Campeonato Latinoamericano/Panamericano";
+eCard13EventDuration.innerText = "Ago 8-10";
+eCard13EventLocation.innerText = "Colombia";
+
+// EVENT CARD #14
+eCard14Year.innerText = "2025";
+eCard14Month.innerText = "Sep";
+eCard14Day.innerText = "19";
+
+eCard14EventName.innerText = "Campeonato Nacional Mexicano de Clubes";
+eCard14EventDuration.innerText = "Sep 19-21";
+eCard14EventLocation.innerText = "Guerrero";
+
+// EVENT CARD #15
+eCard15Year.innerText = "2025";
+eCard15Month.innerText = "Oct";
+eCard15Day.innerText = "11";
+
+eCard15EventName.innerText = "API";
+eCard15EventDuration.innerText = "Oct 11 y 12";
+eCard15EventLocation.innerText = "Nueva Zelanda";
+
+// EVENT CARD #16
+eCard16Year.innerText = "2025";
+eCard16Month.innerText = "Oct";
+eCard16Day.innerText = "18";
+
+eCard16EventName.innerText = "Campamento Selección Nacional";
+eCard16EventDuration.innerText = "Oct 18 y 19";
+eCard16EventLocation.innerText = "";
+
+// EVENT CARD #17
+eCard17Year.innerText = "2025";
+eCard17Month.innerText = "Nov";
+eCard17Day.innerText = "27";
+
+eCard17EventName.innerText = "Campeonato Norteamerican y del Caribe";
+eCard17EventDuration.innerText = "Nov 27-30";
+eCard17EventLocation.innerText = "CDMX";
+
+// // EVENT CARD #18
+// eCard18Year.innerText = "2025";
+// eCard18Month.innerText = "Jul";
+// eCard18Day.innerText = "-";
+
+// eCard18EventName.innerText = "Campeonato Nacional Juvenil";
+// eCard18EventDuration.innerText = "Jul";
+// eCard18EventLocation.innerText = "";
